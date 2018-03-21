db899f40b33958bd702f67f762cd820c96439cf0
Dmitry Ohotnikov <ohotnikoff@bk.ru>
2018-03-21 11:14:51 +0500

    update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo ln -s /home/box/web/etc/gunicorn-wsgi.conf  /etc/gunicorn.d/test
-# sudo ln -s /home/box/web/etc/gunicorn-django.conf  /etc/gunicorn.d/django.conf
+sudo ln -s /home/box/web/etc/gunicorn-wsgi.conf  /etc/gunicorn.d/wsgi.conf
+sudo ln -s /home/box/web/etc/gunicorn-django.conf  /etc/gunicorn.d/django.conf
 sudo /etc/init.d/nginx restart
