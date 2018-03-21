2bc80305bbb5a3dccd8014be63bcba10799747fa
Dmitry Ohotnikov <ohotnikoff@bk.ru>
2018-03-21 09:51:37 +0500

    add files from 14826 step

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
 sudo /etc/init.d/nginx restart
