5d0b2c6d28c33716a0534d0b0f62cb5d2215ab82
Dmitry Ohotnikov <ohotnikoff@bk.ru>
2018-03-21 12:20:08 +0500

    update init.sh

--- init.sh
+++ init.sh
@@ -1,9 +1,6 @@
-# sudo pip install --upgrade django
-# sudo pip install --upgrade gunicorn
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn-wsgi.conf  /etc/gunicorn.d/wsgi
 sudo ln -s /home/box/web/etc/gunicorn-django.conf  /etc/gunicorn.d/django
-sudo /etc/init.d/nginx restart
-# sudo gunicorn -c /home/box/web/etc/gunicorn-wsgi.conf hello:app
-#sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
+sudo /etc/init.d/gunicorn restart
