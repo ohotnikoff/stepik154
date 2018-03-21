40fd5559a7abd91cea1adc075d8363d87558d4ec
Dmitry Ohotnikov <ohotnikoff@bk.ru>
2018-03-21 11:06:21 +0500

    update urls and configs for server

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -22,4 +22,8 @@ urlpatterns = [
     url(r'^$', views.test),
     url(r'^login/$', views.test),
     url(r'^signup/$', views.test),
+    url(r'^question/(\d+)$', views.test),
+    url(r'^ask/$', views.test),
+    url(r'^popular/$', views.test),
+    url(r'^new/$', views.test),
 ]
--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo ln -s /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/gunicorn-wsgi.conf  /etc/gunicorn.d/test
+# sudo ln -s /home/box/web/etc/gunicorn-django.conf  /etc/gunicorn.d/django.conf
 sudo /etc/init.d/nginx restart
