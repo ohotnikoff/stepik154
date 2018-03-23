2d9a2ee06ecc260952763e9455326dd1f5255bc3
Dmitry Ohotnikov <ohotnikoff@bk.ru>
2018-03-23 08:04:40 +0500

    updated settings and models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
-    added_at = models.DateTimeField(blank=True)
+    added_at = models.DateTimeField(blank=True, auto_now_add=True)
     rating = models.IntegerField(default=0)
     author = models.OneToOneField(User)
     likes = models.ManyToManyField(User)
@@ -23,6 +23,6 @@ class QuestionManager(models.Manager):
 
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateTimeField(blank=True)
+    added_at = models.DateTimeField(blank=True, auto_now_add=True)
     question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
     author = models.OneToOneField(User)
