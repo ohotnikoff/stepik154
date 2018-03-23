3f0c5e7aaaea786745419fa42d093aa17a4641b6
Dmitry Ohotnikov <ohotnikoff@bk.ru>
2018-03-23 08:18:07 +0500

    updated models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -18,7 +18,7 @@ class Question(models.Model):
     added_at = models.DateTimeField(blank=True, auto_now_add=True)
     rating = models.IntegerField(default=0)
     author = models.OneToOneField(User)
-    likes = models.ManyToManyField(User)
+    likes = models.ManyToManyField(User, related_name='likes_set')
     objects = QuestionManager()
 
 class Answer(models.Model):
